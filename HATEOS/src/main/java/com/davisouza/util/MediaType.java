49e78e57e32dadb37e3119a65e27c358e129b929
Davasz <davidavi200615@gmail.com>
2023-11-23 21:10:47 -0300

    1

--- HATEOS/src/main/java/com/davisouza/util/MediaType.java
+++
@@ -1,8 +0,0 @@
-package com.davisouza.util;
-
-public class MediaType {
-	
-	public static final String YAML = "application/x-yaml";
-	public static final String JSON = "application/json";
-	public static final String XML = "application/xml";
-}
--- HATEOS/src/main/resources/db/migration/V2__Populate_Table_Person.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `person` VALUES (1, 'Davi', 'Souza', 'Rua Minas Gerais', 'Male')
-
